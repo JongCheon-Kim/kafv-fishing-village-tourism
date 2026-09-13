aca7f01130f8002393b3aa4ce7e393fe8a8b816b
JongCheon-Kim <123dipika@naver.com>
2026-09-13 12:44:57 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kafv-fishing-village-tourism-pwa-intro-v1';
+const CACHE='kafv-fishing-village-tourism-pwa-intro-concentration-v2';
 const ASSETS=["./", "./index.html", "./villages.js", "./config.js", "./kto-area-codes.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./assets/fallback-village.svg", "./assets/fallback-tour.svg", "./assets/fallback-food.svg", "./assets/fallback-stay.svg", "./assets/fallback-experience.svg", "./assets/fallback-pet.svg", "./assets/splash.png", "./assets/main-cover.png"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
